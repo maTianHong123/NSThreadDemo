11b79550309e85d88650255cd0d22f375d9070f2
yu hua <yuhua@yudemac-mini.local>
2018-04-16 09:23:14 +0800

    Initial Commit

---
+++ NSThreadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSThreadDemo
+//
+//  Created by yu hua on 2018/4/16.
+//  Copyright © 2018年 Chongqing Huayu Network Education Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSThreadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSThreadDemo
+//
+//  Created by yu hua on 2018/4/16.
+//  Copyright © 2018年 Chongqing Huayu Network Education Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
